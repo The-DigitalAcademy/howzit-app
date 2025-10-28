98ceb1cffe35d38a50fd277b08d3ef0efd83d3ff
Lesego <lesego.boase23@gmail.com>
2025-10-28 09:24:05 +0200

    file structure

---
+++ src/app/components/regiser/regiser.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegiserComponent } from './regiser.component';
+
+describe('RegiserComponent', () => {
+  let component: RegiserComponent;
+  let fixture: ComponentFixture<RegiserComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RegiserComponent]
+    });
+    fixture = TestBed.createComponent(RegiserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/regiser/regiser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regiser',
+  templateUrl: './regiser.component.html',
+  styleUrls: ['./regiser.component.css']
+})
+export class RegiserComponent {
+
+}
