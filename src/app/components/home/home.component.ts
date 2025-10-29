38f3a2fab506f992df578c0a3abd4508a0d3a050
SiboneloMaphelana <maphelanasibonelo@gmail.com>
2025-10-29 09:21:19 +0200

    authentication service partially working

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +10,23 @@ import { User } from '../../models/user.model';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  users: User[] = []
-  constructor(private userService: UserService) {
+  users: User[] = [];
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.getUsers();
   }
 
   getUsers() {
     this.userService.getUsers().subscribe({
-      next: (users) => this.users = users,
-      error: (err) => console.error('Error fetching users', err)
-    })
+      next: (users) => (this.users = users),
+      error: (err) => console.error('Error fetching users', err),
+    });
+  }
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,58 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
-// import { AuthenticationService } from '../../authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
   });
 
+  errorMessage = '';
+  isLoading = false;
+
+  constructor(private router: Router, private auth: AuthService) {}
+
+  ngOnInit(): void {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  onLogin() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      this.errorMessage = 'Please fill in all fields correctly';
+      return;
+    }
+
+    const email = this.loginForm.value.email || '';
+    const password = this.loginForm.value.password || '';
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.auth.login(email, password).subscribe({
+      next: (success) => {
+        this.isLoading = false;
+        if (success) {
+          this.router.navigate(['/home']);
+        } else {
+          this.errorMessage = 'Invalid email or password';
+        }
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.errorMessage = 'An error occurred. Please try again.';
+        console.error('Login error:', error);
+      }
+    });
+  }
 
 }
